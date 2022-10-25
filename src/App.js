cc34c552a71170241a713b37601765e3928b3e10
GeethasriIrudayasamy <geethasriirudayasamy@gmail.com>
2022-10-25 18:02:06 +0530

    task2:Add_Restaurant_Summary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
-import Header from "./Components/Layout/Header";
+import Header from "./Components/Layout/Header/Header";
+import MealsSummary from "./Components/Meals/MealsSummary";
 
 const App = () => {
     return (
         <Fragment>
             <Header />
+            <MealsSummary />
         </Fragment>
     );
 };
--- src/Components/Layout/Header/Header.js
+++ src/Components/Layout/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
-import HeaderImg from "../../assets/images/imgforHeader.jpg";
-import CartButton from "./CartButton";
+import HeaderImg from "../../../assets/images/imgforHeader.jpg";
+import CartButton from "../Cart/CartButton";
 // import Logo from "../../assets/icons/Logo_As_Icon.png";
 import classes from "./Header.module.css";
 
@@ -18,7 +18,7 @@ const Header = () => {
             </header>
             <div className={classes["main-img"]}>
                 <img
-                    className={classes["main-image-img"]}
+                    className={classes["main-img"]}
                     src={HeaderImg}
                     alt="Delicious food"
                 />
