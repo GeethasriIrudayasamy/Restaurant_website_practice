970ddfe252a0a6a94a2a123e6da0bd8bbfba6acb
GeethasriIrudayasamy <geethasriirudayasamy@gmail.com>
2022-10-28 18:40:46 +0530

    task4:Adding_A_Form

--- src/Components/Meals/MealsList/MealsList.js
+++ src/Components/Meals/MealsList/MealsList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import MealsForm from "../MealsForm/MealsForm";
 import classes from "./MealsList.module.css";
 
 const MealsList = (props) => {
@@ -8,6 +9,9 @@ const MealsList = (props) => {
                 <h2>{props.name}</h2>
                 <div className={classes.description}>{props.description}</div>
                 <div className={classes.price}>{props.price}</div>
+            </div>
+            <div>
+                <MealsForm id={props.id} />
             </div>
         </li>
     );
